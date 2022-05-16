f97bb2e12efba7980e57dbf3d1eb4556bb7eb1f4
Spec27 <shkvara27@gmail.com>
2022-05-16 16:33:17 +0300

    profile

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,16 @@
 import { Container } from "./Container";
+import { Profile } from "./Profile";
+import user from "../data/user.json"
 export const App = () => {
   return (
-    <Container
-    >
-   Початок Дз
+    <Container>
+    <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
     </Container>
   );
 };
