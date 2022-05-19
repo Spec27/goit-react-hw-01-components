8d0e06004844d6931bb25b292d1e5b1c4fe09b2b
Spec27 <shkvara27@gmail.com>
2022-05-19 16:52:59 +0300

    bug fix

---
+++ src/components/Statistics/ColorRandon.js
@@ -0,0 +1,5 @@
+function colorRandon() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+export default colorRandon;
--- src/components/Statistics/ColorRandon.jsx
+++
@@ -1,5 +0,0 @@
-function ColorRandon() {
-    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-  }
-  
-  export default ColorRandon;
